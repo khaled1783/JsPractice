ef5ba751e28a5f1a2fdcac35763f4ce4dfbbfac4
Khaled Hasan <Khaled.hasan@brainstation-23.com>
2025-03-21 19:56:36 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,38 @@
+
+let bsId = "BS1783";
+console.log(bsId);
+
+let myName = "Khaled"
+console.log(myName);
+myName="Khaled Hasan"
+console.log(myName);
+
+const companyName = "Brain Station 23";
+console.log(companyName);
+
+let age = 24;
+console.log(age);
+
+let isTrainee = true;
+console.log(isTrainee);
+
+
+const myInformation ={
+    bsId : "BS1783",
+    myName : "Khaled Hasan",
+    companyName : "Brain Station 23",
+    age : 24,
+    isTrainee : true
+};
+
+console.log(myInformation);
+console.log(myInformation.bsId);
+console.log(myInformation.myName);
+console.log(myInformation.companyName);
+console.log(myInformation.age);
+console.log(myInformation.isTrainee);
+console.log(typeof myName);
+console.log(typeof companyName);
+console.log(typeof age);
+console.log(typeof isTrainee);
+console.log(typeof myInformation);
