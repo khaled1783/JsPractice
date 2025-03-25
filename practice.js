cc994893fa7a9b489d50aa68675dc52a9b82537d
Khaled Hasan <khaled.hasan@brainstation-23.com>
2025-03-25 14:04:45 +0600

    Practiced Operators and Statements

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-
+//variables
 let bsId = "BS1783";
 console.log(bsId);
 
@@ -36,3 +36,85 @@ console.log(typeof companyName);
 console.log(typeof age);
 console.log(typeof isTrainee);
 console.log(typeof myInformation);
+
+
+// Operators
+//Aerthmetic Operators
+let a = 6;
+let b = 5;
+//++a;//7
+//--b;//4
+a++;//6
+b++;5
+
+console.log("a=",a);//6
+console.log("b=",b);//7
+console.log("a+b=",a+b);//13
+console.log("a-b=",a-b);//1
+console.log("a*b=",a*b);//42
+console.log("a/b=",a/b);//1.16667
+console.log("a%b=",a%b);//1
+console.log("a**b=",a**b);//1
+
+
+//Assignment Operators
+let c = 3;
+let d = 4;
+
+sumC = c+=2;//5
+c *=2;//6
+console.log("c=",sumC);//5
+console.log("c=",c);//10
+
+//Comparison Operators and Conditional Statements
+
+if (c===d) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+//Practice
+//Q1 Get user to input a number using prompt("Enter a number").Check if the number is a multiple of 5 or not.
+
+// let num = prompt('Enter a number');
+// if(num % 5 === 0)  {
+
+//     console.log(num ,"is a multiple of 5");
+// }
+
+// else {
+//     console.log(num ,"is not a multiple of 5");
+// }
+
+
+//Q2--Write a code which can give grades to students according to their scores:
+//80-100,A
+//70-89,B
+//60-69,C
+//50-59,D
+//0-49,F
+
+
+let score = Number(prompt("Enter student's score:"));
+let Grade;
+
+if (score >= 80 && score <= 100) {
+    Grade = "A";
+} else if (score >= 70 && score <= 79) {
+    Grade = "B";
+} else if (score >= 60 && score <= 69) {
+    Grade = "C";
+} else if (score >= 50 && score <= 59) {
+    Grade = "D";
+} else if (score >= 0 && score < 50) {
+    Grade = "F";
+} else {
+     Grade = "Invalid score";  
+ }
+
+console.log("Student's Grade is:", Grade);
+
+
+
